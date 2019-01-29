09392cbbca5847d047bb206b1c037ce862426489
rahuljain <rahul.jain1@bhge.com>
2019-01-29 17:46:34 +0530

    Create app foundation using react-native-web

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Text,View, StyleSheet } from "react-native";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <View style={styles.app}>
+        <View style={styles.appHeader}>
+          <Text style={styles.appTitle}>Welcome to React ⚛️</Text>
+        </View>
+        <Text style={styles.appIntro}>
+           Hello world
+        </Text>
+      </View>
+    )
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  app: {
+    flex: 1
+  },
+  appHeader: {
+    flex: 1,
+    backgroundColor: '#222',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appTitle: {
+    fontSize: 16,
+    color: 'white'
+  },
+  appIntro: {
+    flex: 2,
+    fontSize: 30,
+    textAlign: 'center'
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+AppRegistry.registerComponent( 'App' ,() =>App );
+
+AppRegistry.runApplication('App', {rootTag: document.getElementById('root')});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
