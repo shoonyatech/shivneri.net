b9423f9383b379a9816e4fe908a202fe2b6d50d9
rahuljain <rahul.jain1@bhge.com>
2019-07-04 22:40:36 +0530

    removed dependecy related to react native

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "./index.css"
 
-AppRegistry.registerComponent( 'App' ,() =>App );
-
-AppRegistry.runApplication('App', {rootTag: document.getElementById('root')});
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
