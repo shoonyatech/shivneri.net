be7538d51eeda6668c50797ce912c6a051128fee
rahuljain <rahul.jain1@bhge.com>
2019-07-11 15:17:22 +0530

    routing implemetation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from '../src/components/App/App';
+import SignIn from "../src/components/SignIn/SignIn"
+import SignOut from "../src/components/SignOut/SignOut"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+), document.getElementById('root'));
+
+serviceWorker.register();
