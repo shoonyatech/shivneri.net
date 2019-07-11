c475b9bcae96b1010666f7357ac35634283206cc
rahuljain <rahul.jain1@bhge.com>
2019-07-11 15:17:22 +0530

    routing implemetation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React , {Component} from 'react';
 import "./App.scss"
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
     render(){
         return(
+            <Router>
            <div className="App"> 
                <header className="App-header">  
-                header
+               <nav>
+                        <Link to="/" activeClassName="active">Home</Link>
+                        {" | "}
+                        <Link to="/signin" activeClassName="active">Sign In</Link>
+                        {" | "}
+                        <Link to="/signout" activeClassName="active">Sign Out</Link>
+                </nav>
                </header>
                <section className="App-content">
                 <div className="content">
@@ -17,6 +25,7 @@ class App extends Component {
                 footer
                </footer>
            </div>
+           </Router>
         );
     }
 }
