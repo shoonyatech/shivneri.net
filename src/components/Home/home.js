619785e3421f4ec736d70bc536de5ab7216d9eba
rahuljain <rahul.jain1@bhge.com>
2019-09-01 11:26:49 +0530

    navigation routing

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,32 +1,12 @@
-import React , {Component} from 'react';
-import SignIn from "../SignIn/SignIn"
-import SignOut from "../SignOut/SignOut"
-import {BrowserRouter as Router, Route, Link ,Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import SignIn from "../SignIn/SignIn";
+import SignOut from "../SignOut/SignOut";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-class App extends Component {
-    render(){
-        return(
-        <div>
-               <header className="App-header">  
-               <nav>
-                        <Link to="/" activeClassName="active">Home</Link>
-                        {" | "}
-                        <Link to="/signin" activeClassName="active">Sign In</Link>
-                        {" | "}
-                        <Link to="/signout" activeClassName="active">Sign Out</Link>
-                </nav>
-               </header>
-               <section className="App-content">
-                <div className="content">
-                content
-                </div>
-               </section>
-               <footer className="App-footer">
-                footer
-               </footer>
-        </div>
-        )
-    }
+class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
 }
 
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../src/components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "../src/components/App/App";
 import { createBrowserHistory } from "history";
-import {BrowserRouter} from 'react-router-dom';
-import SignIn from "../src/components/SignIn/SignIn"
-import SignOut from "../src/components/SignOut/SignOut"
-import * as serviceWorker from './serviceWorker';
-
-
+import { BrowserRouter, Route } from "react-router-dom";
+import SignIn from "../src/components/SignIn/SignIn";
+import SignOut from "../src/components/SignOut/SignOut";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById('root'));
+  <BrowserRouter>
+    <Route component={App} />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.register();
