e6207fdfda7e501835aac8c14e961a0cc19594f6
rahuljain <rahul.jain1@bhge.com>
2019-09-02 15:47:45 +0530

    facebook integration

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
+import Facebook from "../Facebook/Facebook";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            SignIn
-          </a>
-        </header>
+        Signin page
+        <Facebook />
       </div>
     );
   }
