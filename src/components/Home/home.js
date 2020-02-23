1b3b57f22adc65ce268fab29f77c806f4a28d64d
rahuljain46 <rahuljain820436@gmail.com>
2020-02-23 17:10:31 +0530

    fort listing

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as fortActions from "../../redux/actions/fortActions";
+import FortList from "../FortList"
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { Redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const Home = () => <h1>Oops! Page not found.</h1>;
 
-export default Home;
+class Home extends React.Component {
+
+    componentDidMount() {
+        console.log("##print##")
+        this.props.dispatch(fortActions.loadForts())
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.forts.map((fort) => {
+                        return <FortList fort={fort} />
+                    })
+
+                }
+
+            </div>
+        )
+    }
+
+}
+
+function mapStateToProps(state) {
+    return {
+        forts: state.forts
+    };
+}
+
+export default connect(
+    mapStateToProps
+)(Home);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import "./index.css";
+import { getForts } from "../src/redux/actions/fortActions";
 import configureStore from "./redux/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
 
